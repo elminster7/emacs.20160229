3b9be8fde7121b7bd5e5a3a298bd74babf7d2912
babel <kamase79@daum.net>
2019-06-27 17:50:26 +0900

    helm m-x keybinding

--- layers/+editor/packages.el
+++ layers/+editor/packages.el
@@ -68,7 +68,7 @@
 			             (projectile-mode 1)
 			             (editorconfig-mode 1)))
   (setq tramp-verbose 6)
-  ;;(global-set-key (kbd "M-x") #'helm-M-x)
+  (global-set-key (kbd "M-x") #'helm-M-x)
   (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
   (global-set-key (kbd "C-x C-f") #'helm-find-files)
 
